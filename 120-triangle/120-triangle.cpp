8a2d29b3855916ac39f6c0ef2d53974f0e9076bf
Mayank Jain <81245278+mayankjain25@users.noreply.github.com>
2022-08-17 23:56:43 +0530

    Time: 13 ms (31.14%), Space: 8.8 MB (59.51%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -17,7 +17,28 @@ public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
         
-        vector<vector<int>>dp(n,vector<int>(triangle[n-1].size(),-1));
-        return f(0,0,triangle.size(),triangle,dp);
+        int dp[n][n];
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++) dp[i][j]=0;
+        }
+        // return f(0,0,triangle.size(),triangle,dp);
+        
+        //for the base case
+        for(int j=0;j<n;j++) dp[n-1][j]=triangle[n-1][j];
+        
+        //for tabulation
+        
+        for(int i=n-2;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                int down=0,downRight=INT_MAX;
+                down=triangle[i][j]+dp[i+1][j];
+                downRight=triangle[i][j] + dp[i+1][j+1];
+                dp[i][j]=min(down,downRight);
+                
+            }
+        }
+        
+        return dp[0][0];
 }
 };

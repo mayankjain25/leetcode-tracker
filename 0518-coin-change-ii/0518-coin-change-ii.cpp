1bfab1ba321b0185389cb9b29a2da8e031330793
Mayank Jain <81245278+mayankjain25@users.noreply.github.com>
2022-10-20 21:06:46 +0530

    Time: 73 ms (31.79%), Space: 7.3 MB (76.89%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,28 +1,33 @@
 class Solution {
 public:
-    
-    int coinChange2(int ind, int amt, vector<int>&coins,vector<vector<int>>&dp){
-         if(ind==0){
-           if(amt%coins[ind]==0) return 1;
-            else return 0;
-        }
-        if(dp[ind][amt]!=-1) return dp[ind][amt];
-            
-        int take=0;
-        if(coins[ind]<=amt)
-        take=coinChange2(ind, amt - coins[ind],coins,dp);
-        int notTake=0+coinChange2(ind-1,amt,coins,dp);
-        
-            
-        return dp[ind][amt]=take+notTake;
-    }
-    
-    
+
     int change(int amount, vector<int>& coins) {
 
         int sum=0,n=coins.size();
-        vector<vector<int>>dp(n+1,vector<int>(amount+1,-1));
-        return coinChange2(n-1,amount,coins,dp);
+        // vector<vector<int>>dp(n,vector<int>(amount+1,0));
+        
+        vector<int>prev (amount + 1,0), curr(amount+1,0);
+        
+        for(int amt=0;amt<=amount;amt++){
+            if(amt%coins[0]==0) prev[amt]=1;
+            else prev[amt]=0;
+        }
+        
+        for(int row=1;row<n;row++){
+            curr=prev;
+            for(int col=0;col<=amount;col++){
+                int take=0;
+                if(coins[row]<=col)
+                take=curr[col - coins[row]];
+                int notTake=prev[col];
+
+
+                curr[col]=take+notTake;
+            }
+            prev=curr;
+        }
+        
+        return prev[amount];
         
     }
 };

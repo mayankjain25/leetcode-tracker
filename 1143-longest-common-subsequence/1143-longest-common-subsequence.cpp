62fb54515319ec7cf894f5669bdf1f1eddaf7ce8
Mayank Jain <81245278+mayankjain25@users.noreply.github.com>
2022-12-16 01:07:36 +0530

    Time: 23 ms (79.09%), Space: 13.1 MB (46.60%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,28 +1,28 @@
 class Solution {
+private:
+    int lengthLCS(string s1, string s2, int index1, int index2,vector<vector<int>>&dp){
+        if(index1<0 or index2<0) return 0;
+        if(dp[index1][index2]!=-1) return dp[index1][index2];
+        
+        if(s1[index1]==s2[index2]) return dp[index1][index2]=1 + lengthLCS(s1,s2,index1-1,index2-1,dp);
+        
+        return dp[index1][index2] = max(lengthLCS(s1,s2,index1-1,index2,dp),lengthLCS(s1,s2,index1,index2-1,dp));
+    }
 public:
-   int longestCommonSubsequence(string s1, string s2) 
-    {
-        int n = s1.length();
-        int m = s2.length();
-        vector<vector<int>> dp(n+1,vector<int> (m+1,0));
-
-        // no need to write base case as we already declare our dp as 0
-
-        for(int i=1;i<=n;i++)
-        {
-            for(int j=1;j<=m;j++)
-            {
-                if(s1[i-1]==s2[j-1])
-                {
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                }
-                else
-                {
-                    dp[i][j] = max(dp[i][j-1],dp[i-1][j]);
-                }
+    int longestCommonSubsequence(string s1, string s2) {
+        int n=s1.size(),m=s2.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int j=0;j<=m;j++) dp[0][j]=0;
+        for(int i=0;i<=n;i++) dp[i][0] = 0;
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s1[i-1]==s2[j-1]) dp[i][j]=1 +dp[i-1][j-1];
+        
+                else dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
             }
         }
-
+            
         return dp[n][m];
     }
 };

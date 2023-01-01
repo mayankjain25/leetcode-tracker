40a3d3fec60cf030352fd696d7494b7c6657c05b
Mayank Jain <81245278+mayankjain25@users.noreply.github.com>
2023-01-01 13:36:00 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (53.35%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        unordered_map<char,int>p2i;
+        unordered_map<string,int>w2i;
+        istringstream in(s);
+        int i=0;
+        int n = pattern.size();
+        
+        for(string word; in>>word;i++){
+            if(i==n or p2i[pattern[i]]!=w2i[word]) return false;
+            p2i[pattern[i]] = w2i[word] = i+1;
+        }
+        
+        return i==n;
+        
+    }
+};

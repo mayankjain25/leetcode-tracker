c001652e923430418eff1491c40b5c256fc433da
Mayank Jain <81245278+mayankjain25@users.noreply.github.com>
2023-01-09 13:01:42 +0530

    Time: 2701 ms (15.67%), Space: 5.9 MB (39.60%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.cpp
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -3,7 +3,7 @@ public:
     bool checkPerfectNumber(int num) {
         int sum=0;
         for(int i=1;i<num;i++){
-            sum+=num%i==0?i:0;
+           if(num%i==0) sum+=i;
         }
         
         return sum==num?true:false;

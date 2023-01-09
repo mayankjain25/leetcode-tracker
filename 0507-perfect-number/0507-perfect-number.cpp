eb3cd3ef82f4e7635ac89d30c4b865191ef5281e
Mayank Jain <81245278+mayankjain25@users.noreply.github.com>
2023-01-09 13:20:23 +0530

    Time: 0 ms (100.00%), Space: 6 MB (6.50%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.cpp
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     bool checkPerfectNumber(int num) {
-        int sum=0;
-        for(int i=1;i<num;i++){
-           if(num%i==0) sum+=i;
-            if(sum>num) break;
+        int sum=1;
+        
+        for(int i=2;i*i<=num;i++){
+            if(num%i==0){
+                sum+=i + (i==(num/i)?0:num/i);
+            }
         }
         
-        return sum==num?true:false;
+        cout<<sum;
+        
+        return sum==num and num!=1;
     }
 };

6f6ec41f90ef548421a0f4b11f6cb693b73d4c8f
Mayank Jain <81245278+mayankjain25@users.noreply.github.com>
2023-01-17 18:59:34 +0530

    Time: 50 ms (48.61%), Space: 20 MB (25.06%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
@@ -11,20 +11,12 @@ public:
             else ones[i]=(i==0?0:ones[i-1]);
         }
         
-        for(int i=n-1;i>=0;i--){
-            if(s[i]=='0'){
-                zero[i]=(i==n-1?0:zero[i+1])+1;
-            }
-            else{
-                zero[i]=(i==n-1?0:zero[i+1]);
-            }
-        }
-        
+
         int curr,ans=INT_MAX;
         
-        for(int i=0;i<n;i++){
+        for(int i=0;i<=n;i++){
             curr=(i==0?0:ones[i-1]);
-            curr+=(i==n-1?0:zero[i+1]);
+            curr+=(n-i) - (ones[n-1] -( i==0?0:ones[i-1]));
             ans=min(ans,curr);
         }
         

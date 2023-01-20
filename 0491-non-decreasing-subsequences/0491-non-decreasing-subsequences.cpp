4a9efdd6e8cc143dbd1687c8a8161382b8975031
Mayank Jain <81245278+mayankjain25@users.noreply.github.com>
2023-01-20 10:58:59 +0530

    Time: 79 ms (67.00%), Space: 22.3 MB (70.61%) - LeetHub

---
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    
+    void helper(int ind,vector<int>&nums, vector<int>&temp,set<vector<int>>&v1){
+        
+        if(ind>=nums.size()){
+            if(temp.size()>=2) v1.insert(temp);
+            return;
+            
+        }
+        
+        if(temp.size()==0 || nums[ind]>=temp.back()){
+            temp.push_back(nums[ind]);
+            helper(ind+1,nums,temp,v1);
+            temp.pop_back();
+        }
+        helper(ind+1,nums,temp,v1);
+        
+        
+    }
+    
+    vector<vector<int>> findSubsequences(vector<int>& nums) {
+        vector<int>temp;
+        set<vector<int>>v1;
+        helper(0,nums,temp,v1);
+        
+        return vector<vector<int>>(v1.begin(),v1.end());
+        
+    }
+};

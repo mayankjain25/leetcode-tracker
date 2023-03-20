add6eb8a4df3c2bc2a28be48e683c810d1215343
Mayank Jain <81245278+mayankjain25@users.noreply.github.com>
2023-03-20 16:46:06 +0530

    Time: 3 ms (98.93%), Space: 14.6 MB (9.3%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,46 +1,37 @@
 class Solution {
 public:
-    void bfs(int sr, int sc, int color, vector<vector<int>>&image,vector<vector<bool>>&vis){
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int n = image.size(), m = image[0].size();
+        vector<vector<bool>>vis(n,vector<bool>(m,false));
+        vector<vector<int>>ans;
+
         queue<pair<int,int>>q;
         q.push({sr,sc});
-        int initColor = image[sr][sc];
-        image[sr][sc] = color;
-        int n = image.size(),m = image[0].size();
+
         vis[sr][sc] = true;
+        // image[sr][sc] = color;
 
         while(!q.empty()){
             auto val = q.front();
-            cout<<val.first<<" "<<val.second<<endl;
             q.pop();
 
-            //traversing the 4 directions
+            vector<int>delrow={-1,0,1,0},delcol={0,1,0,-1};
 
-            for(int delrow = -1;delrow<=1;delrow++){
-                for(int delcol=-1;delcol<=1;delcol++){
-                    if((delrow==-1 and delcol==-1) or (delrow==-1 and delcol==1) or (delrow==1 and delcol==-1) or (delrow==1 and delcol==1)) 
-                    continue;
+            for(int i=0;i<4;i++){
+                int nr = val.first + delrow[i];
+                int nc = val.second + delcol[i];
 
-                    int newRow = val.first + delrow;
-                    int newCol = val.second + delcol;
-
-                    if(newRow>=0 and newRow<n and newCol>=0 and newCol<m and image[newRow][newCol]==initColor and !vis[newRow][newCol]){
-                        image[newRow][newCol] = color;
-                        q.push({newRow,newCol});
-                        vis[newRow][newCol] = true;
-                    }
+                if(nr>=0 and nr<n and nc>=0 and nc<m and !vis[nr][nc] and image[nr][nc]==image[sr][sc]){
+                    q.push({nr,nc});
+                    vis[nr][nc]=true;
+                    image[nr][nc] = color;
                 }
             }
 
-            
+
         }
 
-        // image[sr][sc] = color; 
-    }
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int n = image.size(),m = image[0].size();
-        vector<vector<bool>>vis(n,vector<bool>(m,false));
-
-        bfs(sr, sc, color,image,vis);
+        image[sr][sc]=color;
 
         return image;
     }

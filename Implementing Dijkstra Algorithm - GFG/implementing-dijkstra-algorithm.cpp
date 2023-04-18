4bfdb1a6d7bd5e84c78bea51f30342a02287a728
Mayank Jain <81245278+mayankjain25@users.noreply.github.com>
2023-04-18 14:28:36 +0530

    Added solution - LeetHub

---
+++ Implementing Dijkstra Algorithm - GFG/implementing-dijkstra-algorithm.cpp
@@ -0,0 +1,83 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+	public:
+	//Function to find the shortest distance of all the vertices
+    //from the source vertex S.
+    vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
+    {
+        // Code here
+        
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq; //dist, node
+        vector<int>dist(V, 1e9);
+        
+        dist[S] = 0;
+        
+        pq.push({0,S});
+        
+        while(!pq.empty()){
+            int startingDist = pq.top().first;
+            int currNode  = pq.top().second;
+            pq.pop();
+            
+            for(auto node:adj[currNode]){
+                    
+                    int new_dist = node[1];
+                    int vertex = node[0];
+                    // cout<<vertex<<endl;
+                    if(startingDist + new_dist < dist[vertex]){
+                        dist[vertex] = startingDist + new_dist;
+                        pq.push({dist[vertex],vertex});
+                    }
+
+            }
+        }
+        
+        return dist;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--) {
+        int V, E;
+        cin >> V >> E;
+        vector<vector<int>> adj[V];
+        int i=0;
+        while (i++<E) {
+            int u, v, w;
+            cin >> u >> v >> w;
+            vector<int> t1,t2;
+            t1.push_back(v);
+            t1.push_back(w);
+            adj[u].push_back(t1);
+            t2.push_back(u);
+            t2.push_back(w);
+            adj[v].push_back(t2);
+        }
+        int S;
+        cin>>S;
+        
+        Solution obj;
+    	vector<int> res = obj.dijkstra(V, adj, S);
+    	
+    	for(int i=0; i<V; i++)
+    	    cout<<res[i]<<" ";
+    	cout<<endl;
+    }
+
+    return 0;
+}
+
+
+// } Driver Code Ends

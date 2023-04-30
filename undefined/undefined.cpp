8443c60ba4b861ef7336a984a935f30dbb6e2331
Mayank Jain <81245278+mayankjain25@users.noreply.github.com>
2023-05-01 01:23:45 +0530

    Time: 60 ms, Memory: 70.6 MB - LeetHub

--- undefined/undefined.cpp
+++ undefined/undefined.cpp
@@ -1,27 +1,14 @@
 class Solution {
 public:
-    int minNumber(vector<int>& nums1, vector<int>& nums2) {
+    int maximizeSum(vector<int>& nums, int k) {
+        int maxi = *max_element(nums.begin(),nums.end());
+        int sum = 0;
         
-        map<int,int>m;
-        
-        for(int i=0;i<nums1.size();i++){
-            m[nums1[i]]++;
+        while(k--){
+            sum += maxi;
+            maxi++;
         }
         
-        for(int i=0;i<nums2.size();i++){
-            m[nums2[i]]++;
-        }
-        
-        for(auto it:m){
-            
-            if(it.second>1) return it.first;
-            
-            }
-             
-        int n1=*min_element(nums1.begin(),nums1.end()),n2=*min_element(nums2.begin(),nums2.end());
-        if(n1<n2) return n1*10 + n2;
-        else return n2*10 + n1;
-             
-        
+        return sum;
     }
 };
